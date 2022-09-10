d59221e6cf14b1fbf44f7faa7b2e3b2899633dbc
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2022-09-10 09:01:57 +0530

    Area of square, rectangle or traingle

--- practical8.cpp
+++ practical8.cpp
@@ -7,63 +7,69 @@ function overloading.
 
 #include <iostream.h>
 #include <conio.h>
+#include <math.h>
 
 class Area
 {
-    public:
-    double l, b;
-    double s, s1, s2, s3;
-    double area;
-    double area();
+  public:
+  double area, s;
+
+	// Functions are written under class itself  to avoid errors of parameters.
+	double Area::CalcArea(double l) // For square
+	{
+		area = l*l;
+		return area;
+	}
+
+	double Area::CalcArea(double l, double b) // For rectangle
+	{
+		area = l * b;
+		return area;
+	}
+
+	double Area::CalcArea(double s1, double s2, double s3) // For triangle
+	{
+		s = (s1 + s2 + s3) / 2;
+		area = sqrt(s*(s-s1)*(s-s2)*(s-s3));
+		return area;
+	}
 };
 
-double area(l) // For square
-{
-    area = l*l;
-    return area;
-}
-
-double area(l, b) // For rectangle
-{
-    area = l * b;
-    return area;
-}
-
-double area(s1, s2, s3) // For triangle
-{
-    s = (s1 + s2 + s3) / 2;
-
-}
 void main()
 {
+    clrscr();
+    double l, b;
+    double s1, s2, s3;
     Area obj; // Creating obj of Area class
-    int choice
+    int choice;
     cout << "Choices" << endl;
     cout << "1. Square " << endl;
     cout << "2. Rectangle " << endl;
     cout << "3. Triangle " << endl;
     cout << "Enter your choice (1/2/3) : ";
     cin >> choice;
+
     switch(choice)
     {
         case 1: cout << "You have chosen square. " << endl;
         cout << "Enter side : ";
         cin >> l;
-        cout << "Area of square = " << obj.area(l) << endl;
+        cout << "Area of square = " << obj.CalcArea(l) << endl;
         break;
 
         case 2: cout << "You have chosen rectangle. " << endl;
         cout << "Enter length and breadth : ";
         cin >> l >> b;
-        cout << "Area of rectangle = " << obj.area(l, b) << endl;
+        cout << "Area of rectangle = " << obj.CalcArea(l, b) << endl;
         break;
 
         case 3: cout << "You have chosen triangle. " << endl;
         cout << "Enter side1, side2, side3 : ";
-        cin >> l >> b;
-        cout << "Area of Triangle = " << obj.area(sl, s2, s3) << endl;
+        cin >> s1 >> s2 >> s3;
+        cout << "Area of Triangle = " << obj.CalcArea(s1, s2, s3) << endl;
         break;
 
         default : cout << "Invalid choice! " << endl;
     }
+    getch();  
 }
